1fed3d16f084217f87fb26ef6e56db81d9f7195c
SLX-0823 <93922126+SLX-0823@users.noreply.github.com>
2024-10-07 17:28:57 +0800

    Changes to register.js
    fix some errors and added a simple feature that allows navigation back to the login screen

--- register.js
+++ register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import Logo from './assets/logo.svg';
 import colors from './colors';
@@ -40,11 +40,85 @@ const RegisterScreen = () => {
     }
 
 
+    const handleRegister = () => {
+        // Add your login logic here
+        navigation.navigate('Login'); // for now just navigate to main menu
+      };
+
+
     return (
         <View style={styles.container}>
-            <Text>Register</Text>
+            <View style={styles.headerContainer}>
+                <Logo width={100} height={100} />
+                <Text style={styles.header}>Orangutan Oasis</Text>
+            </View>
+
+            <View style={styles.headerContainer}>
+                <Text style={styles.title}>Register</Text>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+                    <Text style={styles.buttonText}>
+                        Back to Login
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 16,
+      backgroundColor: colors.background,
+    },
+    headerContainer: {
+      marginTop: 30,
+      marginLeft: -10,
+      flex: 1,
+      flexDirection: 'row',
+      marginBottom: -150,
+    },
+    header: {
+      marginTop: 30,
+      fontSize: 35,
+      color: colors.text,
+      fontFamily: 'Alike',
+    },
+    main: {
+      flex: 1,
+      marginTop: -50,
+      justifyContent: 'flex-start',
+    },
+    input: {
+      height: 40,
+      borderColor: colors.secondary,
+      borderWidth: 1,
+      marginBottom: 12,
+      paddingHorizontal: 8,
+      color: colors.text,
+      fontFamily: 'SegoeUI',
+    },
+    title: {
+      fontSize: 24,
+      marginBottom: 16,
+      color: colors.primary,
+      fontFamily: 'Alike',
+    },
+    button: {
+      backgroundColor: colors.primary, // Use your color variable for the button background
+      padding: 10,
+      alignItems: 'center',
+      borderRadius: 5,
+      height: 36,
+    },
+    buttonText: {
+      color: colors.text, // Change this to your desired text color
+      fontSize: 16,
+      fontFamily: 'SegoeUI',
+    },
+  });
 
 export default RegisterScreen;
